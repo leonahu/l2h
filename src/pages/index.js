ec8096d3771f6232315cc700d8e548418bf80e1c
Leona Hu Hudelson <hqleona@gmail.com>
2017-08-01 17:05:31 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+
+const IndexPage = () => (
+  <div>
+    <h1>Ok</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <img src="http://lorempixel.com/400/200/" />
+    <Link to="page-2">Go to page 1</Link>
+    <Link to="post">Go to post 2</Link>
+
+
+
+  </div>
+)
+
+export default IndexPage
---
+++ src/pages/post.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+class Post extends React.Component {
+  render() {
+    return
+    <div>Hello Class Component</div>
+  }
+}
+
+export default Post
